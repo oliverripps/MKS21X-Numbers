ca8d25fa242977702195eaa775c660eb2d0b551f
Oliver Ripps <oripps@stuy.edu>
2018-10-15 20:06:54 -0400

    final at home

---
+++ RationalNumber.java
@@ -0,0 +1,32 @@
+public class RationalNumber extends RealNumber{
+  /*private int X;
+  private int Y;
+  public RationalNumber(int x,int y){
+    x=X;
+    y=Y;
+  }*/
+  public boolean notundefined(RationalNumber a){
+    return true;
+//Y can't be 0
+  }
+  public double simplified(RationalNumber a){
+    return 1.0;
+  //X over Y as a double(include reducing of decimals if over 4))
+  }
+  public double realvsrational(RationalNumber a, RealNumber b){
+    return 1.1;
+  //Double of which is bigger using simplified(not reduced?)
+  }
+  public String getrational(RationalNumber a){
+    return "imemotionaldriventhough";
+//return X / Y
+  }
+  public String getsimplified(RationalNumber a){
+    return "Ihaveacomplexpersonalitythough";
+//return simplified(double)
+  }
+  public void ifoneorzero(RationalNumber a){
+//return 0 or 1 if denominator is 1 or numerator 0
+  }
+
+}
---
+++ RealNumber.java
@@ -0,0 +1,15 @@
+public class RealNumber{
+  /*private double X;
+
+  public RealNumber(double x){
+    X=x;
+  }*/
+  public double isgreater(RealNumber a,RealNumber b){
+    return 1.0;
+    //whichever is bigger
+  }
+  public double getreal(RealNumber a){
+    return 1.1;
+  }
+
+}
